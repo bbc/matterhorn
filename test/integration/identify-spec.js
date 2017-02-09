4d1648b20d545bfbf0c6f8dee31b6139dbff1a83
Adam Walters <adam.walters@bbc.co.uk>
2017-02-09 09:32:10 +0000

    IPTV-2996 added test for brand endpoint

---
+++ test/integration/identify-spec.js
@@ -0,0 +1,19 @@
+const req = require('supertest')
+
+const app = require('../../src/app')
+const request = req(app.listen())
+
+describe('Identify', () => {
+  describe('if given a user agent', () => {
+    it('Returns a brand and model key', (done) => {
+      request.get('/identify/json?ua=some-user-agent')
+        .end((err, res) => {
+          if (err) throw err
+          expect(res.status).toBe(200)
+          expect(res.body.brand).toBeDefined()
+          expect(res.body.model).toBeDefined()
+          done()
+        })
+    })
+  })
+})
