15264fef74846e3115e0fdb3e395d84196827c5b
Adam Walters <adam.walters@bbc.co.uk>
2017-02-09 11:52:51 +0000

    IPTV-2996 add caching around network requests to endpoint

--- src/middleware/device-identification/whoami.js
+++ src/middleware/device-identification/whoami.js
@@ -1,25 +1,33 @@
 const R = require('ramda')
 
 const request = require('../../common/request-promise')
+const deviceCache = {}
 
 function identifyDeviceByWhoami (whoami) {
-  return request.get('https://connected-tv.files.bbci.co.uk/tvp-whoami/data/json')
-    .then((body, res) => {
-      const error = {
-        error: true,
-        code: 404,
-        message: {
-          make: 'unknown',
-          model: 'unknown'
+  return new Promise((resolve, reject) => {
+    if (deviceCache[whoami]) return resolve(deviceCache[whoami])
+    return request.get('https://connected-tv.files.bbci.co.uk/tvp-whoami/data/json')
+      .then((body, res) => {
+        const error = {
+          error: true,
+          code: 404,
+          message: {
+            make: 'unknown',
+            model: 'unknown'
+          }
         }
-      }
-      const allDevices = body.body
-      const matchPattern = item => new RegExp(item.who_am_i_pattern).test(whoami)
-      const filterMatches = R.filter(matchPattern)
-      const firstMatch = R.head
-      const device = R.compose(firstMatch, filterMatches)(allDevices)
-      return device || error
-    })
+        const allDevices = body.body
+        const matchPattern = item => new RegExp(item.who_am_i_pattern).test(whoami)
+        const filterMatches = R.filter(matchPattern)
+        const firstMatch = R.head
+        const device = R.compose(firstMatch, filterMatches)(allDevices)
+        if (device) {
+          deviceCache[whoami] = device
+          return resolve(device)
+        }
+        return reject(error)
+      })
+  })
 }
 
 module.exports = identifyDeviceByWhoami
