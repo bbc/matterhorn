6208c7d50d5720620a15aec5e2191e3d90e2624e
Omair Qadir <omair.qadir.ext@bbc.co.uk>
2017-02-20 13:51:01 +0000

    Poll for device identification data every 5 minutes

--- src/middleware/device-identification/device-data.js
+++ src/middleware/device-identification/device-data.js
@@ -3,14 +3,14 @@ const request = require('../../common/request-promise')
 let devices
 
 function makeRequest() {
-  return request.get('https://connected-tv.files.bbci.co.uk/device-identification-data/data.json')
-    .then(response => {
-      devices = response.body
-      req.deviceData = devices
-      return next()
-    })
+  request.get('https://connected-tv.files.bbci.co.uk/device-identification-data/data.json')
+  .then(response => {
+    devices = response.body
+  })
 }
-setTimeout(makeRequest, 5000)
+
+setInterval(makeRequest, 300000);
+makeRequest();
 
 module.exports = function (req, res, next) {
   if (devices) {
