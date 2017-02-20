25937bdb8d71079184206d57a5999cac2d1028eb
Alex Chesters <alex.chesters@bbc.co.uk>
2017-02-20 14:49:28 +0000

    IPTV-2982 Add logging for unknown user-agent

--- src/middleware/device-identification/user-agent.js
+++ src/middleware/device-identification/user-agent.js
@@ -1,9 +1,15 @@
 const melanite = require('melanite')
 
+const logger = require('../../common/logger')
+
 function identifyDeviceByUserAgent (req, res) {
   const match = melanite.match(req.deviceData)
   const device = match(req.params.ua)
-  return device ? res.json(device) : res.status(404).json({brand: null, model: null})
+  if (!device) {
+    logger.warn('Request received for unknown user-agent:', req.params.ua)
+    return res.status(404).json({brand: null, model: null})
+  }
+  return res.json(device)
 }
 
 module.exports = identifyDeviceByUserAgent
