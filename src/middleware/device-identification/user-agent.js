74bda08862b3af7e9bbdf2da9cb9ad06171c4ffd
Alex Chesters <alex.chesters@bbc.co.uk>
2017-02-20 13:13:14 +0000

    IPTV-2982 Use melantie to identify based on UA

--- src/middleware/device-identification/user-agent.js
+++ src/middleware/device-identification/user-agent.js
@@ -1,5 +1,10 @@
+const melanite = require('melanite')
+
+const stubData = require('../../../data/dummy-data.json')
+
 function identifyDeviceByUserAgent (req, res) {
-  return res.json({brand: 'brand', model: 'model'})
+  const match = melanite.match(stubData)
+  return res.json(match(req.params.ua))
 }
 
 module.exports = identifyDeviceByUserAgent
