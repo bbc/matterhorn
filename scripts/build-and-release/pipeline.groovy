31294f26f041fdeda64262b91019f5ed2db088fc
Alex Chesters <alex.chesters@bbc.co.uk>
2017-03-13 14:10:19 +0000

    Update pipeline

--- scripts/build-and-release/pipeline.groovy
+++ scripts/build-and-release/pipeline.groovy
@@ -1,6 +1,22 @@
-node {
-    stage('Checkout repo') {
-        git 'git@github.com:bbc/matterhorn.git'
+pipeline {
+    agent {
+        label 'centos7'
     }
-  }
+    stages {
+        stage('Checkout repo') {
+            steps {
+                git 'git@github.com:bbc/matterhorn.git'   
+            }
+        }
+        stage('Install dependencies') {
+            steps {
+                sh 'npm install'   
+            }
+        }
+        stage('Run the tests') {
+            steps {
+                sh 'npm test'
+            }
+        }
+    }
 }
