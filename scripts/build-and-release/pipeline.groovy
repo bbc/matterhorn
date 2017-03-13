e9559df9b10473161c5480f3d95481cedf90fc0a
Alex Chesters <alex.chesters@bbc.co.uk>
2017-03-13 15:07:04 +0000

    Fix indentation

--- scripts/build-and-release/pipeline.groovy
+++ scripts/build-and-release/pipeline.groovy
@@ -1,25 +1,25 @@
 pipeline {
-    agent {
-        label 'centos7'
+  agent {
+    label 'centos7'
+  }
+  tools {
+    nodejs 'LTS'
+  }
+  stages {
+    stage('Install dependencies') {
+      steps {
+        sh 'npm install'
+      }
     }
-    tools {
-        nodejs 'LTS'
-    }
-    stages {
-        stage('Install dependencies') {
-            steps {
-                sh 'npm install'
-            }
-        }
-        stage('Run the tests') {
-            steps {
-                sh 'npm test'
-            }
-        }
-        stage('Create a release') {
-          steps {
-            sh 'npm run release'
+    stage('Run the tests') {
+      steps {
+        sh 'npm test'
           }
-        }
+      }
+    stage('Create a release') {
+      steps {
+        sh 'npm run release'
+      }
     }
+  }
 }
