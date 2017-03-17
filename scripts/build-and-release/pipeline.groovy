82f1e5075588076545184b3c80312bb4d6270db7
Alex Chesters <alex.chesters@bbc.co.uk>
2017-03-17 14:04:49 +0000

    Add comment

--- scripts/build-and-release/pipeline.groovy
+++ scripts/build-and-release/pipeline.groovy
@@ -2,9 +2,15 @@ pipeline {
   agent {
     label 'centos7'
   }
+  // The nodejs tool needs to be updated to support Jenkins pipelines
+  // However, in order to do that we need to migrate away from the Multijob
+  // plugin as it causes a conflict. Until then, we can live with the fact
+  // that the CentOS 7 agent has Node 6.9.4 enabled by default
+  /*
   tools {
     nodejs 'LTS'
   }
+  */
   stages {
     stage('Install dependencies') {
       steps {
