e4fd0a14dc3914a53f3dd989233b59a634cf7db5
Adam Cook <Adam.Cook@bbc.co.uk>
2017-04-19 08:31:05 +0100

    Change logger.log -> logger.info

--- src/common/log-response-time.js
+++ src/common/log-response-time.js
@@ -1,6 +1,6 @@
-const logger = require('../../common/logger')
+const logger = require('./logger')
 
 module.exports = function (startTime) {
   const timeTaken = Date.now() - startTime
-  logger.log(`Matterhorn request served in: ${timeTaken}ms`)
+  logger.info(`Matterhorn request served in: ${timeTaken}ms`)
 }
