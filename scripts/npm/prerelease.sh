8230fc55180b31b1b3cce73e3ccadf57c0270800
Alex Chesters <AlexChesters@users.noreply.github.com>
2019-07-24 14:01:30 +0100

    rename

--- scripts/npm/prerelease.sh
+++ scripts/npm/prerelease.sh
@@ -4,5 +4,5 @@ set -e
 VERSION=$(node -e "console.log(require('./package.json').version)")
 speculate --release $VERSION_`git rev-parse --short HEAD`
 mkdir -p /root/rpmbuild/SOURCES/
-cp SOURCES/matterhorn.tar.gz /root/rpmbuild/SOURCES/matterhorn.tar.gz
-rpmbuild -ba SPECS/matterhorn.spec
+cp SOURCES/matterhorn-wargame.tar.gz /root/rpmbuild/SOURCES/matterhorn-wargame.tar.gz
+rpmbuild -ba SPECS/matterhorn-wargame.spec
