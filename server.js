4c1511f6981d309bd6b1a0dd56ac6aa24f97e15a
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2021-07-01 13:02:28 +0100

    BADHORSE-501 use all available cores

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
+const cluster = require('cluster')
+const os = require('os')
+
 const app = require('./src/app')
 const logger = require('./src/common/logger')
 
 const { port = 8080 } = require('minimist')(process.argv.slice(2))
 
-app.listen(port, (req, res) => {
-  logger.info(`[MATTERHORN] listening at http://localhost:${port}`)
-})
+if (cluster.isMaster) {
+  os.cpus().map(() => cluster.fork())
+
+  cluster.on('online', worker => logger.info(`Worker ${worker.id} started`))
+
+  cluster.on('exit', worker => {
+    cluster.fork()
+    return logger.error(`Worker ${worker.id} died`)
+  })
+} else {
+  app.listen(port, (req, res) => {
+    logger.info(`[MATTERHORN] listening at http://localhost:${port}`)
+  })
+}
